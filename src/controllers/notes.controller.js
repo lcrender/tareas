b573ddb342043db15d357fe7ff7545fc3b22ef26
Alejandro Chazarreta <lcrender@gmail.com>
2023-01-02 04:10:53 +0100

    crud completo

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -13,11 +13,15 @@ notesCtrl.renderNotes = async (req, res) => {
     const notes = await Note.find().lean()
     res.render('notes/all-notes', {notes})
 };
-notesCtrl.renderEditForm = (req, res) => {
-    res.send('render edit form');
+notesCtrl.renderEditForm = async (req, res) => {
+    const note = await Note.findById(req.params.id).lean();
+    res.render('notes/edit-note', {note});
+  
 };
-notesCtrl.updateNote = (req, res) => {
-    res.send('update note');
+notesCtrl.updateNote = async (req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, {title, description}).lean();
+    res.redirect('/notes');
 };
 notesCtrl.deleteNote = async (req, res) => {
     await Note.findByIdAndDelete(req.params.id)
