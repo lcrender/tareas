a7cb5a2168581df5e58ed25fb8cd02e4f7e28dd1
Alejandro Chazarreta <lcrender@gmail.com>
2023-01-02 01:15:58 +0100

    rutas

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,25 @@
+const {Router}  = require('express');
+const router = Router();
+const { 
+    renderNoteForm,
+    createNewNote,
+    renderNotes,
+    renderEditForm,
+    updateNote,
+    deleteNote } = require('../controllers/notes.controller')
+
+// Nueva nota
+router.get('/notes/add', renderNoteForm)
+router.post('/notes/add', createNewNote)
+
+// Obtener todas las notas
+router.get('/notes', renderNotes)
+
+// Editar nota
+router.get('/notes/edit/:id', renderEditForm)
+router.put('/notes/edit/:id', updateNote)
+
+// Borrar Nota
+router.delete('/notes/delete/:id', deleteNote)
+
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Rutas
 app.use(require('./routes/index.routes'));
+app.use(require('./routes/notes.routes'));
 
 // Archivos Estaticos
 app.use(express.static(path.join(__dirname, 'public')));
